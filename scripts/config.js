69a6e2ec208397bd10599826995a8d8c2a1ef7e2
Sukjoon Kim <sjoonk@gmail.com>
2022-02-21 11:23:52 +0900

    align tab space

--- scripts/config.js
+++ scripts/config.js
@@ -2,7 +2,7 @@
 require('dotenv').config()
 
 module.exports = {
-	privateKey: process.env.PRIVATE_KEY,
+  privateKey: process.env.PRIVATE_KEY,
   klaytnNodeUrl: process.env.KLAYTN_NODE_URL,
   tokenBaseUri: process.env.TOKEN_BASE_URI,
   contractAddress: process.env.CONTRACT_ADDRESS
